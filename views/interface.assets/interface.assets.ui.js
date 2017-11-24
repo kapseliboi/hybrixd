e24eb2893746e388f53aa2bfd2ceb3489b763651
lucavh <lucaverhees@gmail.com>
2017-11-24 12:01:27 +0100

    Modal and alert scroll fixes

--- views/interface.assets/interface.assets.ui.js
+++ views/interface.assets/interface.assets.ui.js
@@ -57,6 +57,22 @@ UItransform = {
       }
 }
 
+$(".clearable").each(function() {
+  
+  var $inp = $(this).find("input:text"),
+      $cle = $(this).find(".clearable__clear");
+
+  $inp.on("input", function(){
+    $cle.toggle(!!this.value);
+  });
+  
+  $cle.on("touchstart click", function(e) {
+    e.preventDefault();
+    $inp.val("").trigger("input");
+  });
+  
+});
+
 displayAssets = function displayAssets() {
   balance = {}
   balance.asset = [];
