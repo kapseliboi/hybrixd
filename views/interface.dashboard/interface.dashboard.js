0eaec1919555f71785fd95ba286fa40837aea965
Luca Verhees <lucaverhees@vpn-stud-146-50-148-101.vpn.uva.nl>
2018-03-28 23:11:53 +0200

    added styling for favourite assets dashboard and fixed hybrid icon size

--- views/interface.dashboard/interface.dashboard.js
+++ views/interface.dashboard/interface.dashboard.js
@@ -131,7 +131,7 @@ function displayAssets() {
                       var icon = (symbolName in black.svgs) ? black.svgs[symbolName] : mkSvgIcon(symbolName);
 
                       var str = asset.starred
-                          ? acc.str + '<div onclick="fetchview(\'interface.assets\',{user_keys: pass_args.user_keys, nonce: pass_args.nonce, asset:\'' + asset.id + '\', element: \'' + assetElementID + '\'});" class="balance">' + '<div class="icon">' + icon + '</div>' + '<h5>'+ asset.id + '</h5><div class="divider"></div><h3 class="balance balance-' + hyphenizedID + '">' + progressbar()+'</h3></div>'
+                          ? acc.str + '<div onclick="fetchview(\'interface.assets\',{user_keys: pass_args.user_keys, nonce: pass_args.nonce, asset:\'' + asset.id + '\', element: \'' + assetElementID + '\'});" class="balance">' + '<div class="icon">' + icon + '</div>' + '<h5>'+ asset.id + '</h5><h3 class="balance balance-' + hyphenizedID + '">' + progressbar()+'</h3></div>'
                           : acc.str;
                       return {i: acc.i + 1, str: str};
                     }
