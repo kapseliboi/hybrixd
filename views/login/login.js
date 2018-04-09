b5de5a0ced46e16643979031420051366c0e3f48
“Marco <marco.alkema@gmail.com>
2018-04-09 09:19:21 +0200

    Factor up keyboard and click events

--- views/login/login.js
+++ views/login/login.js
@@ -4,17 +4,24 @@ const A = animations;
 const Utils = utils;
 
 Utils.documentReady(function () {
-  document.querySelector('#loginbutton').onclick = handleLogin;
-  document.querySelector('#inputUserID').onkeypress = focusOnPasswordAfterReturnKeyOnID;
-  document.querySelector('#inputPasscode').onkeypress = focusOnLoginButton(handleLogin);
-  document.keydown = handleCtrlSKeyEvent; // for legacy wallets enable signin button on CTRL-S
-
+  initializeClickAndKeyEvents();
   maybeOpenNewWalletModal();
 });
 
-init.login = function(args) {
-  if ( DEBUG ) { console.log('init.login called with args: ' + JSON.stringify(args)); }
-  // do nothing
+function handleLogin () {
+  var btnIsNotDisabled = !document.querySelector('#loginbutton').classList.contains('disabled');
+  if (btnIsNotDisabled) {
+    // \/\/\/\/ CANNOT FACTOR THIS UP YET, BECAUSE IT WILL NOT FIND CREDENTIALS RIGHT AWAY.
+    var userid = document.querySelector('#inputUserID').value.toUpperCase();
+    var passcode = document.querySelector('#inputPasscode').value;
+    var isValidUserIDAndPassword = C.validateUserIDLength(userid) && C.validatePasswordLength(passcode);
+    if (isValidUserIDAndPassword) {
+      var sessionStep = session_step = 0;
+      A.rotateLogin(0);
+      setCSSTorenderButtonsToDisabled();
+      main(userid, passcode, sessionStep);
+    }
+  }
 }
 
 function main (userid, passcode, sessionStep) {
@@ -28,8 +35,8 @@ function main (userid, passcode, sessionStep) {
 function instantiateNaclAndHandleLogin (passcode, userID, sessionStep, cb) {
   return function (naclinstance) {
     nacl = naclinstance; // TODO: INSTANTIATION MUST BE SAVED SOMEWHERE NON-GLOBALLY SO IT IS ACCESSABLE THROUGHOUT
-    generateNonceAndUserKeys(passcode, userID, sessionStep)
-  }
+    generateNonceAndUserKeys(passcode, userID, sessionStep);
+  };
 }
 
 function generateNonceAndUserKeys (passcode, userID, sessionStep) {
@@ -37,7 +44,7 @@ function generateNonceAndUserKeys (passcode, userID, sessionStep) {
   var userKeys = C.generateKeys(passcode, userID, 0);
   var user_pubkey = nacl.to_hex(userKeys.boxPk);
 
-  startAnimationAndDoLogin(userKeys, nonce, userID, sessionStep)
+  startAnimationAndDoLogin(userKeys, nonce, userID, sessionStep);
 }
 
 function startAnimationAndDoLogin (userKeys, nonce, userID, sessionStep) {
@@ -64,15 +71,15 @@ function processSessionStep0Reply (sessionStep0Data, nonce, userKeys) {
     if (cleanNonceHasCorrectLength) {
       session_step++; // next step, hand out nonce2
       const sessionStep = session_step;
-      var sessionStep1Data = C.generateSecondarySessionData(nonce1Data, sessionStep0Data.session_hexkey, sessionStep0Data.session_signpair.signSk)
-      var url = path+'x/' + sessionStep0Data.session_hexsign + '/' + sessionStep + '/' + sessionStep1Data.crypt_hex;
-        fetch(url)
+      var sessionStep1Data = C.generateSecondarySessionData(nonce1Data, sessionStep0Data.session_hexkey, sessionStep0Data.session_signpair.signSk);
+      var url = path + 'x/' + sessionStep0Data.session_hexsign + '/' + sessionStep + '/' + sessionStep1Data.crypt_hex;
+      fetch(url)
         .then(r => r.json()
               .then(postSession1StepData(sessionStep0Data, sessionStep1Data, nonce, userKeys))
               .catch(e => console.log('postSessionStep0Data: Error retrieving nonce:', e)))
         .catch(e => console.log('postSessionStep0Data: Error fetching data:', e));
     }
-  }
+  };
 }
 
 function setSessionDataInElement (sessionHex) {
@@ -85,12 +92,12 @@ function postSession1StepData (initialSessionData, sessionStep1Data, nonce, user
     A.dialLogin(2);
     C.sessionStep1Reply(data, sessionData, setSessionDataInElement);
     A.dialLogin(3);
-  }
+  };
 }
 
 function maybeOpenNewWalletModal () {
-  if (location.href.indexOf("#") != -1) {
-    var locationHref = location.href.substr(location.href.indexOf("#"));
+  if (location.href.indexOf('#') !== -1) {
+    var locationHref = location.href.substr(location.href.indexOf('#'));
     if (locationHref === '#new') {
       PRNG.seeder.restart();
       document.getElementById('newaccountmodal').style.display = 'block';
@@ -102,33 +109,31 @@ function cannotSetUpEncryptedSessionAlert () {
   console.log('Error: Cannot set up encrypted session. Please check your connectivity!');
 }
 
-function sessionContinuation (user_keys, nonce, userid) {
+function sessionContinuation (userKeys, nonce, userid) {
   return function () {
-    if ( DEBUG ) { console.log(readSession(user_keys, nonce, sessionData, cannotSetUpEncryptedSessionAlert)) } // use read_session(user_keys,nonce) to read out session variables
-    setTimeout(function() { // added extra time to avoid forward to interface before x authentication completes!
-      fetchview('interface',{
-        user_keys,
+    if (DEBUG) { console.log(readSession(userKeys, nonce, sessionData, cannotSetUpEncryptedSessionAlert)); } // use read_session(user_keys,nonce) to read out session variables
+    setTimeout(function () { // added extra time to avoid forward to interface before x authentication completes!
+      fetchview('interface', {
+        user_keys: userKeys,
         nonce,
         userid
       });
-    }, 3000 );
-  }
+    }, 3000);
+  };
 }
 
 function getSessionData () {
-  return document.querySelector('#session_data').textContent
+  return document.querySelector('#session_data').textContent;
 }
 
 function handleCtrlSKeyEvent (e) {
-  var key = undefined;
   var possible = [ e.key, e.keyIdentifier, e.keyCode, e.which ];
 
   while (key === undefined && possible.length > 0) {
     key = possible.pop();
   }
 
-  if (key && (key == '115' || key == '83' ) && (e.ctrlKey || e.metaKey) && !(e.altKey))
-  {
+  if (key && (key === '115' || key === '83') && (e.ctrlKey || e.metaKey) && !(e.altKey)) {
     e.preventDefault();
     document.querySelector('#loginbutton').removeAttribute('disabled');
     return false;
@@ -138,15 +143,15 @@ function handleCtrlSKeyEvent (e) {
 
 function setCSSTorenderButtonsToDisabled () {
   const arcBackgroundColor = document.querySelector('#combinator').style.color;
-  document.querySelector('#loginbutton').classList.add('disabled')
+  document.querySelector('#loginbutton').classList.add('disabled');
   document.querySelector('#arc0').style.backgroundColor = arcBackgroundColor;
-  document.querySelector('#generatebutton').setAttribute('disabled','disabled');
-  document.querySelector('#helpbutton').setAttribute('disabled','disabled');
+  document.querySelector('#generatebutton').setAttribute('disabled', 'disabled');
+  document.querySelector('#helpbutton').setAttribute('disabled', 'disabled');
   document.querySelector('#combinatorwrap').style.opacity = 1;
 }
 
 function focusOnPasswordAfterReturnKeyOnID (e) {
-  const keyPressIsReturn = e.keyCode == 13
+  const keyPressIsReturn = e.keyCode === 13;
   if (keyPressIsReturn) {
     document.querySelector('#inputPasscode').focus();
   }
@@ -154,26 +159,17 @@ function focusOnPasswordAfterReturnKeyOnID (e) {
 
 function focusOnLoginButton (cb) {
   return function (e) {
-    const keyPressIsReturn = e.keyCode == 13
+    const keyPressIsReturn = e.keyCode === 13;
     if (keyPressIsReturn) {
       document.querySelector('#loginbutton').focus();
       cb();
     }
-  }
-};
+  };
+}
 
-function handleLogin () {
-  var btnIsNotDisabled = !document.querySelector('#loginbutton').classList.contains('disabled');
-  if (btnIsNotDisabled) {
-    // \/\/\/\/ CANNOT FACTOR THIS UP YET, BECAUSE IT WILL NOT FIND CREDENTIALS RIGHT AWAY.
-    var userid = document.querySelector('#inputUserID').value.toUpperCase();
-    var passcode = document.querySelector('#inputPasscode').value;
-    var isValidUserIDAndPassword = C.validateUserIDLength(userid) && C.validatePasswordLength(passcode);
-    if (isValidUserIDAndPassword) {
-      var sessionStep = session_step = 0;
-      A.rotateLogin(0);
-      setCSSTorenderButtonsToDisabled()
-      main(userid, passcode, sessionStep);
-    }
-  }
+function initializeClickAndKeyEvents () {
+  document.querySelector('#loginbutton').onclick = handleLogin;
+  document.querySelector('#inputUserID').onkeypress = focusOnPasswordAfterReturnKeyOnID;
+  document.querySelector('#inputPasscode').onkeypress = focusOnLoginButton(handleLogin);
+  document.keydown = handleCtrlSKeyEvent; // for legacy wallets enable signin button on CTRL-S
 }
