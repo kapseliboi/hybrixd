82e17adf364cdd3e5b12b24b9c36696f6aed2de1
Luca Verhees <lucaverhees@gmail.com>
2018-06-13 15:52:49 +0200

    #160-styling for broweser support notifications

--- views/login/login.js
+++ views/login/login.js
@@ -280,7 +280,7 @@ function checkBrowserSupport () {
   var isAnyUnsupportedBrowser = R.any(isUnsupportedBrowser, unsupportedBrowsers);
 
   if (isAnyUnsupportedBrowser) {
-    document.querySelector('#loginform').innerHTML = '<h2>MEH!<h2/><p>Your browser is not supported...</p><p>To use this awesome product you must have at least an Apple Macbook Pro from 2018!!!</p>';
+    document.querySelector('#loginform').innerHTML = '<h2 class="loginbox-title">Your browser is not supported</h2><p class="loginbox-text">We’re sorry, but you’re using a browser we don’t support. To use the Internet of Coins wallet, please upgrade to the latest version of <a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank">Firefox</a>, <a href="https://www.google.com/chrome/" target="_blank">Chrome</a>, <a href="https://support.apple.com/downloads/safari" target="_blank">Safari</a> or <a href="https://www.microsoft.com/en-us/download/internet-explorer.aspx" target="_blank">Internet Explorer</a></p><p class="loginbox-text">Please visit <a href="https://internetofcoins.org/faq#browser-support" target="_blank">our FAQ</a> for more information about browser support. </p>';
     document.querySelector('#generateform').classList.add('inactive');
     document.querySelector('#helpbutton').classList.add('inactive');
     document.querySelector('#alertbutton').classList.add('inactive');
