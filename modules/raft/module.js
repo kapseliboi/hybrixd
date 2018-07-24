a3e5b77e09d0fdcd25a273d162691881dcbc3f68
Rouke Pouw <roukepouw@gmail.com>
2018-07-24 14:08:55 +0000

    Issue/227 various fixes to asset status, balances

--- modules/raft/module.js
+++ modules/raft/module.js
@@ -3,6 +3,8 @@
 // Module to provide storage
 
 // required libraries in this context
+var IoC = require('../../common/ioc.client/ioc.nodejs.client');
+var ioc = new IoC.Interface({http: require('http')});
 
 // exports
 exports.init = init;
@@ -10,64 +12,72 @@ exports.exec = exec;
 
 var peers = [];
 // initialization function
-function init() {
-
+function init () {
 }
 
 // exec
-function exec(properties) {
+function exec (properties) {
   // decode our serialized properties
   var processID = properties.processID;
-  //var source = properties.source;
+  // var source = properties.source;
   var command = properties.command;
   var subprocesses = [];
   // set request to what command we are performing
   global.hybridd.proc[processID].request = properties.command;
 
   // handle standard cases here, and construct the sequential process list
-  switch(command[0]){
-  case 'peers' :
-    subprocesses.push('stop(0,'+JSON.stringify(peers)+')');
-    break;
-  case 'cluster' :
-    var xpath = command.slice(1);
+  switch (command[0]) {
+    case 'peers' :
 
-    for(var peer in peers){
-      subprocesses.push("curl('http://"+peers[peer]+"','/"+xpath.join('/')+"','GET',null,{},{autoproc:true})");
-    }
-    subprocesses.push("coll("+peers.length+")");
+      ioc.sequential([
+        'init',
+        {username: 'POMEW4B5XACN3ZCX', password: 'TVZS7LODA5CSGP6U'}, 'login',
+        {host: 'http://localhost:1111/'}, 'addHost',
+        {symbol: 'dummy'}, 'addAsset',
+        {symbol: 'dummy', amount: 100}, 'transaction'
+      ]
+        , () => { console.log('Succes'); }
+        , (error) => { console.error(error); }
+      );
 
-    break;
-  case 'clusterstatus' :
-    for(var peer in peers){
-      subprocesses.push("curl('http://"+peers[peer]+"','/e/raft/peers','GET',null,{},{autoproc:true})");
-    }
-    subprocesses.push("coll("+peers.length+")");
+      subprocesses.push('stop(0,' + JSON.stringify(peers) + ')');
+      break;
+    case 'cluster' :
+      var xpath = command.slice(1);
 
-    break;
-  case 'add' :
-    var peer = command[1];
-    if(peers.indexOf(peer)===-1){
-      peers.push(peer);
-      subprocesses.push('stop(0,"Peer added.")');
-    }else{
-      subprocesses.push('stop(0,"Peer already added.")');
-    }
-    break;
-  case 'remove' :
-    var peer = command[1];
-    var index = peers.indexOf(peer);
-    if(index===-1){
-      subprocesses.push('stop(0,"Peer not in cluster.")');
-    }else{
-      peers.splice(index,1);
-      subprocesses.push('stop(0,"Peer removed.")');
-    }
+      for (var peer in peers) {
+        subprocesses.push("curl('http://" + peers[peer] + "','/" + xpath.join('/') + "','GET',null,{},{autoproc:true})");
+      }
+      subprocesses.push('coll(' + peers.length + ')');
 
+      break;
+    case 'clusterstatus' :
+      for (var peer in peers) {
+        subprocesses.push("curl('http://" + peers[peer] + "','/e/raft/peers','GET',null,{},{autoproc:true})");
+      }
+      subprocesses.push('coll(' + peers.length + ')');
 
+      break;
+    case 'add' :
+      var peer = command[1];
+      if (peers.indexOf(peer) === -1) {
+        peers.push(peer);
+        subprocesses.push('stop(0,"Peer added.")');
+      } else {
+        subprocesses.push('stop(0,"Peer already added.")');
+      }
+      break;
+    case 'remove' :
+      var peer = command[1];
+      var index = peers.indexOf(peer);
+      if (index === -1) {
+        subprocesses.push('stop(0,"Peer not in cluster.")');
+      } else {
+        peers.splice(index, 1);
+        subprocesses.push('stop(0,"Peer removed.")');
+      }
   }
 
-
   // fire the Qrtz-language program into the subprocess queue
-  scheduler.fire(processID,subprocesses);
+  scheduler.fire(processID, subprocesses);
 }
--- modules/wavalidator/module.js
+++ modules/wavalidator/module.js
@@ -6,41 +6,45 @@
 exports.init = init;
 exports.exec = exec;
 
-
 // https://github.com/ognus/wallet-address-validator
-var WAValidator
+var WAValidator;
 
 // initialization function
-function init() {
+function init () {
 //  modules.initexec('validator',["init"]);
   WAValidator = require('wallet-address-validator');
 }
 
-function exec(properties) {
-
+function exec (properties) {
   var command = properties.command;
   var processID = properties.processID;
-  console.log("command"+ JSON.stringify(command));
+  console.log('command' + JSON.stringify(command));
   var subprocesses = [];
   // set request to what command we are performing
   global.hybridd.proc[processID].request = properties.command;
 
   var symbol = command[0].toUpperCase();
   var address = command[1];
-
-  try{
-    var valid = WAValidator.validate(address,symbol);//'1KFzzGtDdnq5hrwxXGjwVnKzRbvf8WVxck', 'BTC'
-
-
-    if(valid){
+  if (symbol === 'DUMMY') {
+    if (address === '_dummyaddress_') {
       subprocesses.push("stop(0,'valid')");
-    }else{
+    } else {
       subprocesses.push("stop(0,'invalid')");
     }
-  }catch(e){
-    subprocesses.push("stop(1,'Symbol is not supported by wallet-address-validator')");
+  } else {
+    try {
+      var valid = WAValidator.validate(address, symbol);// '1KFzzGtDdnq5hrwxXGjwVnKzRbvf8WVxck', 'BTC'
+
+      if (valid) {
+        subprocesses.push("stop(0,'valid')");
+      } else {
+        subprocesses.push("stop(0,'invalid')");
+      }
+    } catch (e) {
+      subprocesses.push("stop(1,'Symbol is not supported by wallet-address-validator')");
+    }
   }
 
   // fire the Qrtz-language program into the subprocess queue
-  scheduler.fire(processID,subprocesses);
+  scheduler.fire(processID, subprocesses);
 }
