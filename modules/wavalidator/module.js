3a9ae81152b52f50fbde7744a337f887e85cd334
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2018-08-01 19:07:56 +0200

    Add validation

--- modules/wavalidator/module.js
+++ modules/wavalidator/module.js
@@ -14,7 +14,6 @@ function init () {
 //  modules.initexec('validator',["init"]);
   WAValidator = require('wallet-address-validator');
 }
-
 function exec (properties) {
   var command = properties.command;
   var processID = properties.processID;
@@ -24,8 +23,58 @@ function exec (properties) {
 
   var symbol = command[0].toUpperCase();
   var address = command[1];
+
+  if (symbol === 'UBQ' || symbol === 'EXP') { symbol = 'ETH'; }
+  if (symbol === 'XCP') { symbol = 'BTC'; }
+
   if (symbol === 'DUMMY') {
     if (address === '_dummyaddress_') {
+      subprocesses.push("stop(0,'valid')");
+    } else {
+      subprocesses.push("stop(0,'invalid')");
+    }
+  } else if (symbol === 'NXT') {
+    if (address.length === 24 && address.startsWith('NXT-')) {
+      // TODO fully implement https://bitcoin.stackexchange.com/questions/30341/programmatically-validating-nxt-addresses
+      subprocesses.push("stop(0,'valid')");
+    } else {
+      subprocesses.push("stop(0,'invalid')");
+    }
+  } else if (symbol === 'XEL') {
+    if (address.length === 24 && address.startsWith('XEL-')) {
+      // TODO fully implement https://bitcoin.stackexchange.com/questions/30341/programmatically-validating-nxt-addresses
+      subprocesses.push("stop(0,'valid')");
+    } else {
+      subprocesses.push("stop(0,'invalid')");
+    }
+  } else if (symbol === 'BURST') {
+    if (address.length === 20 || (address.length === 26 && address.startsWith('BURST-'))) {
+      // TODO fully implement https://bitcoin.stackexchange.com/questions/30341/programmatically-validating-nxt-addresses
+      subprocesses.push("stop(0,'valid')");
+    } else {
+      subprocesses.push("stop(0,'invalid')");
+    }
+  } else if (symbol === 'ARK') {
+    if (address.length === 34 && address.startsWith('A')) {
+      // TODO improve
+      subprocesses.push("stop(0,'valid')");
+    } else {
+      subprocesses.push("stop(0,'invalid')");
+    }
+  } else if (symbol === 'LSK') {
+    if ((address.length === 21 || address.length === 20) && address.endsWith('L') && /^\d+$/.test(address.substr(0, address.length - 1))) {
+      subprocesses.push("stop(0,'valid')");
+    } else {
+      subprocesses.push("stop(0,'invalid')");
+    }
+  } else if (symbol === 'SHIFT') {
+    if ((address.length === 21 || address.length === 20) && address.endsWith('S') && /^\d+$/.test(address.substr(0, address.length - 1))) {
+      subprocesses.push("stop(0,'valid')");
+    } else {
+      subprocesses.push("stop(0,'invalid')");
+    }
+  } else if (symbol === 'RISE') {
+    if ((address.length === 21 || address.length === 20) && address.endsWith('R') && /^\d+$/.test(address.substr(0, address.length - 1))) {
       subprocesses.push("stop(0,'valid')");
     } else {
       subprocesses.push("stop(0,'invalid')");
