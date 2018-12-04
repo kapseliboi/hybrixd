eefc4ab189fc228b079021e5488f7465a0c3fd83
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2018-12-04 17:14:28 +0100

    More renames

--- modules/storage/storage.js
+++ modules/storage/storage.js
@@ -128,10 +128,10 @@ var autoClean = function () {
               // DEBUG: console.log(" [i] module storage: test on storage " + fileelement);
               if (fs.existsSync(fileelement)) {
                 var meta = JSON.parse(String(fs.readFileSync(fileelement)));
-                // DEPRECATED: var mindeadline = Date.now() - (global.hybridd.maxstoragetime * 86400) - global.hybridd.maxstoragetime * (864 * meta.n);
-                var mindeadline = Date.now() - ((typeof global.hybridd.minstoragetime !== 'undefined' && global.hybridd.minstoragetime >= 1 ? global.hybridd.minstoragetime : 1) * 86400);
-                var maxdeadline = Date.now() - ((typeof global.hybridd.maxstoragetime !== 'undefined' && global.hybridd.maxstoragetime >= 1 ? global.hybridd.maxstoragetime : 365) * 86400);
-                if ((meta.res!==1 && meta.time < mindeadline) || (meta.res===1 && meta.time < maxdeadline)) {
+                // DEPRECATED: var mindeadline = Date.now() - (global.hybrixd.maxstoragetime * 86400) - global.hybrixd.maxstoragetime * (864 * meta.n);
+                var mindeadline = Date.now() - ((typeof global.hybrixd.minstoragetime !== 'undefined' && global.hybrixd.minstoragetime >= 1 ? global.hybrixd.minstoragetime : 1) * 86400);
+                var maxdeadline = Date.now() - ((typeof global.hybrixd.maxstoragetime !== 'undefined' && global.hybrixd.maxstoragetime >= 1 ? global.hybrixd.maxstoragetime : 365) * 86400);
+                if ((meta.res !== 1 && meta.time < mindeadline) || (meta.res === 1 && meta.time < maxdeadline)) {
                   var dataelement = fileelement.substr(0, fileelement.length - 5);
                   try {
                     fs.unlinkSync(dataelement);
