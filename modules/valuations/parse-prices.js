c937014b7e64cae854d6d85f93cf2f9ceeb8381a
Rouke Pouw <roukepouw@gmail.com>
2019-01-30 12:35:31 +0000

    Issue/473 fix downloads for valuations

--- modules/valuations/parse-prices.js
+++ modules/valuations/parse-prices.js
@@ -1,7 +1,7 @@
+var scheduler = require('../../lib/scheduler');
+
 var fs = require('fs');
 var xmldoc = require('xmldoc');
-
-var folder = __dirname + '/var/prices';
 
 function addQuote (accumulator, quote_currency, base_currency, price) {
   // Don't add quote if the input fails these requirements
@@ -16,17 +16,20 @@ function addQuote (accumulator, quote_currency, base_currency, price) {
   return accumulator;
 }
 
-function parseEUCentralBank (folder) {
-  filename = '/EUCentralBank-prices.xml';
-  name = 'EUCentralBank';
+function parseEUCentralBank (xmlString) {
+  var name = 'EUCentralBank';
 
-  var document = new xmldoc.XmlDocument(fs.readFileSync(folder + filename, 'utf8'));
-  xmlElements = document.children[5].children[1].children.filter(node => node.constructor.name === 'XmlElement');
-
-  quote_accumulator = {};
+  var xmlElements;
+  try {
+    var document = new xmldoc.XmlDocument(xmlString);
+    xmlElements = document.children[5].children[1].children.filter(node => node.constructor.name === 'XmlElement');
+  } catch (e) {
+    return {name: name, quotes: {}};
+  }
+  var quote_accumulator = {};
   xmlElements.map(function (key, index) {
-    price = key.attr.rate;
-    quote_currency = key.attr.currency;
+    var price = key.attr.rate;
+    var quote_currency = key.attr.currency;
 
     quote_accumulator = addQuote(quote_accumulator, 'EUR', quote_currency, price);
     quote_accumulator = addQuote(quote_accumulator, quote_currency, 'EUR', 1 / price);
@@ -34,15 +37,15 @@ function parseEUCentralBank (folder) {
   return {name: name, quotes: quote_accumulator};
 }
 
-function parseCoinmarketcap (folder) {
-  filename = '/coinmarketcap-prices.json';
-  name = 'coinmarketcap';
-  var obj = JSON.parse(fs.readFileSync(folder + filename, 'utf8'));
-
-  quote_accumulator = {};
+function parseCoinmarketcap (obj) {
+  var name = 'coinmarketcap';
+  var quote_accumulator = {};
+  if (!obj) {
+    return {name, quotes: quote_accumulator};
+  }
   Object.keys(obj.data).map(function (key, index) {
-    price = obj.data[key].quotes.USD.price;
-    quote_currency = obj.data[key].symbol;
+    var price = obj.data[key].quotes.USD.price;
+    var quote_currency = obj.data[key].symbol;
 
     quote_accumulator = addQuote(quote_accumulator, 'USD', quote_currency, 1 / price);
     quote_accumulator = addQuote(quote_accumulator, quote_currency, 'USD', price);
@@ -50,14 +53,15 @@ function parseCoinmarketcap (folder) {
   return {name: name, quotes: quote_accumulator};
 }
 
-function parseCoinbase (folder) {
-  filename = '/coinbase-prices.json';
-  name = 'coinbase';
-  var obj = JSON.parse(fs.readFileSync(folder + filename, 'utf8'));
+function parseCoinbase (obj) {
+  var name = 'coinbase';
+  var quote_accumulator = {};
+  if (!obj) {
+    return {name, quotes: quote_accumulator};
+  }
 
-  quote_accumulator = {};
   Object.keys(obj.data.rates).map(function (key, index) {
-    price = parseFloat(obj.data.rates[key]);
+    var price = parseFloat(obj.data.rates[key]);
 
     quote_accumulator = addQuote(quote_accumulator, 'USD', key, price);
     quote_accumulator = addQuote(quote_accumulator, key, 'USD', 1 / price);
@@ -65,47 +69,46 @@ function parseCoinbase (folder) {
   return {name: name, quotes: quote_accumulator};
 }
 
-function parseBinance (folder) {
-  filename = '/binance-prices.json';
-  name = 'binance';
-  baseCurrencies = ['BTC', 'ETH', 'USDT', 'BNB'];
-
-  var obj = JSON.parse(fs.readFileSync(folder + filename, 'utf8'));
-
-  quote_accumulator = {};
+function parseBinance (obj) {
+  var name = 'binance';
+  var baseCurrencies = ['BTC', 'ETH', 'USDT', 'BNB'];
+  var quote_accumulator = {};
+  if (!obj) {
+    return {name, quotes: quote_accumulator};
+  }
   Object.keys(obj).map(function (key, index) {
-    symbolPair = obj[key].symbol;
+    var symbolPair = obj[key].symbol;
 
-    base_currency = baseCurrencies.find(function (currency) { return symbolPair.endsWith(currency); });
-    quote_currency = symbolPair.slice(0, symbolPair.length - base_currency.length);
-    price = parseFloat(obj[key].price);
+    var base_currency = baseCurrencies.find(function (currency) { return symbolPair.endsWith(currency); });
+    if (base_currency) {
+      var quote_currency = symbolPair.slice(0, symbolPair.length - base_currency.length);
+      var price = parseFloat(obj[key].price);
 
-    quote_accumulator = addQuote(quote_accumulator, quote_currency, base_currency, price);
-    quote_accumulator = addQuote(quote_accumulator, base_currency, quote_currency, 1 / price);
+      quote_accumulator = addQuote(quote_accumulator, quote_currency, base_currency, price);
+      quote_accumulator = addQuote(quote_accumulator, base_currency, quote_currency, 1 / price);
+    }
   });
   return {name: name, quotes: quote_accumulator};
 }
 
-function parseHitbtc (folder) {
-  price_file = '/hitbtc-prices.json';
-  symbol_file = '/hitbtc-symbols.json';
-  name = 'hitbtc';
+function parseHitbtc (prices, symbols) {
+  var name = 'hitbtc';
+  var quote_accumulator = {};
+  if (!prices || !symbols) {
+    return {name, quotes: quote_accumulator};
+  }
 
-  var symbols = JSON.parse(fs.readFileSync(folder + symbol_file, 'utf8'));
-  var prices = JSON.parse(fs.readFileSync(folder + price_file, 'utf8'));
-
-  symbols_obj = {};
+  var symbols_obj = {};
   symbols.map(function (key, index) {
     symbols_obj[key.id] = key;
   });
 
-  quote_accumulator = {};
   prices.map(function (key, index) {
-    exchange_symbol = symbols_obj[key['symbol']];
+    var exchange_symbol = symbols_obj[key['symbol']];
 
-    base_currency = exchange_symbol['baseCurrency'];
-    quote_currency = exchange_symbol['quoteCurrency'];
-    price = parseFloat(key.last);
+    var base_currency = exchange_symbol['baseCurrency'];
+    var quote_currency = exchange_symbol['quoteCurrency'];
+    var price = parseFloat(key.last);
 
     quote_accumulator = addQuote(quote_accumulator, base_currency, quote_currency, price);
     quote_accumulator = addQuote(quote_accumulator, quote_currency, base_currency, 1 / price);
@@ -151,10 +154,18 @@ function updateMinAndMedians (exchangeRates) {
   return exchangeRates;
 }
 
-function parse () {
-  var sourcesOut = combineQuoteSources([parseEUCentralBank(folder), parseHitbtc(folder), parseBinance(folder), parseCoinmarketcap(folder), parseCoinbase(folder)]);
-  return updateMinAndMedians(sourcesOut);
+function parse (data) {
+  var processID = data.processID;
+  var sourcesOut = combineQuoteSources([
+    parseEUCentralBank(data.EUCentralBank),
+    parseHitbtc(data.hitbtc_symbols, data.hitbtc_prices),
+    parseBinance(data.binance),
+    parseCoinmarketcap(data.coinmarketcap),
+    parseCoinbase(data.coinbase)
+  ]);
+  var result = updateMinAndMedians(sourcesOut);
+
+  scheduler.pass(processID, 0, result);
 }
-// console.dir(getQuotes().USD.quotes.TRY, { depth: null });
 
 exports.parse = parse;
