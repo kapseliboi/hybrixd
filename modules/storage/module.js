5e16a81eadfc9b23366c31a5df9ab76b22d7e721
Rouke Pouw <roukepouw@gmail.com>
2019-02-19 13:41:19 +0000

    Modernize storage module

--- modules/storage/module.js
+++ modules/storage/module.js
@@ -3,114 +3,37 @@
 // Module to provide storage
 
 // required libraries in this context
-var storage = require('./storage'); // key-value storage
-var execSync = require('child_process').execSync;
-var scheduler = require('../../lib/scheduler');
+let storage = require('./storage'); // key-value storage
 
-// initialization function
-function init () {
-  // check for storage directory? if not there make one
-  execSync('mkdir -p ../storage'); // TODO replace with Nodejs platform independant version TODO use conf
+function cron (proc) {
+  storage.autoClean();
+  proc.pass('Autoclean');
 }
 
-// exec
-function exec (properties) {
-  // decode our serialized properties
-  var processID = properties.processID;
-  // var source = properties.source;
-  var command = properties.command;
-  var subprocesses = [];
-  // set request to what command we are performing
-  global.hybrixd.proc[processID].request = properties.command;
-  // handle standard cases here, and construct the sequential process list
-  switch (command[0]) {
-    case 'cron' :
-      storage.autoClean();
-      subprocesses.push('done("Autoclean")');
-      break;
-    case 'load':
-    case 'get':
-      subprocesses.push('type "file:data"');
-      subprocesses.push('func "get" {key:"' + command[1] + '"}');
-      subprocesses.push('file');
-      break;
-    case 'save':
-    case 'set': // stores data and returns proof of work to be solved
-      subprocesses.push('func("set",{key:"' + command[1] + '", value:"' + command[2] + '"})');
-      break;
-    case 'seek': // stores data and returns proof of work to be solved
-      subprocesses.push('func("seek",{key:"' + command[1] + '"})');
-      break;
-    case 'work':
-    case 'pow':
-      subprocesses.push('func("pow",{key:"' + command[1] + '", pow:"' + command[2] + '"})');
-      break;
-    case 'burn':
-    case 'del':
-      subprocesses.push('fail("Deleting data from node storage is not supported!")');
-      break;
-    case 'meta':
-      subprocesses.push('func("meta",{key:"' + command[1] + '"})');
-      break;
-  }
-  // fire the Qrtz-language program into the subprocess queue
-  scheduler.fire(processID, subprocesses);
+function seek (proc, data) {
+  storage.seek(data.key, proc.pass, proc.fail);
 }
 
-var get = function (properties) {
-  var processID = properties.processID;
-  storage.get(properties.key,
-    value => { scheduler.stop(processID, 0, value); },
-    error => { scheduler.stop(processID, 1, error); }
-  );
-};
+function meta (proc, data) {
+  storage.getMeta(data.key, proc.pass, proc.fail);
+}
 
-var qrtzLoad = function (properties) {
-  var processID = properties.processID;
-  storage.qrtzLoad(properties.key,
-    value => { scheduler.pass(processID, 0, value); },
-    error => { scheduler.pass(processID, 1, error); }
-  );
-};
+function load (proc, data) {
+  storage.get(data.key, proc.pass, proc.fail);
+}
 
-var seek = function (properties) {
-  var processID = properties.processID;
-  storage.seek(properties.key,
-    value => { scheduler.stop(processID, 0, value); },
-    error => { scheduler.stop(processID, 1, error); }
-  );
-};
+function save (proc, data) {
+  storage.set({key: data.key, value: data.value}, proc.pass, proc.fail);
+}
 
-var set = function (properties) {
-  var processID = properties.processID;
-  storage.set({key: properties.key, value: properties.value},
-    hash => { scheduler.stop(processID, 0, hash); },
-    error => { scheduler.stop(processID, 1, error); }
-  );
-};
-
-var pow = function (properties) {
-  var processID = properties.processID;
-  storage.provideProof({key: properties.key, pow: properties.pow},
-    response => { scheduler.stop(processID, 0, response); },
-    error => { scheduler.stop(processID, 1, error); }
-  );
-};
-
-var meta = function (properties) {
-  var processID = properties.processID;
-  storage.getMeta(properties.key,
-    meta => { scheduler.stop(processID, 0, meta); },
-    error => { scheduler.stop(processID, 1, error); }
-  );
-};
+function work (proc, data) {
+  storage.provideProof({key: data.key, pow: data.pow}, proc.pass, proc.fail);
+}
 
 // exports
-exports.init = init;
-exports.exec = exec;
-exports.get = get;
-exports.set = set;
+exports.save = save;
+exports.load = load;
+exports.work = work;
 exports.seek = seek;
-exports.pow = pow;
+exports.cron = cron;
 exports.meta = meta;
-//exports.load = qrtzLoad;
