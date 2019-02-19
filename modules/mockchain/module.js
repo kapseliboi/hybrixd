9991e21b3ae856c200fd5f92f8126a9397eb9cb0
Rouke Pouw <roukepouw@gmail.com>
2019-02-19 11:07:33 +0000

    Issue/506 clean up validation and update module

--- modules/mockchain/module.js
+++ modules/mockchain/module.js
@@ -1,136 +1,169 @@
 // (C) 2015 Internet of Coins / Metasync / Joachim de Koning
-// hybrixd module - storage/module.js
-// Module to provide clustering
+// hybrixd module - mockchain/module.js
+// Module to provide a mock chain for testing
 
-var scheduler = require('../../lib/scheduler');
-var fs = require('fs');
+let fs = require('fs');
+
 // exports
-exports.init = init;
-exports.exec = exec;
+exports.mine = mine;
+exports.balance = balance;
+exports.push = push;
+exports.history = history;
+exports.transaction = transaction;
 
-// initialization function
-function init () {
-}
+const filePath = '../../test.mockchain.json';
 
-// exec
-function exec (properties) {
-  var filePath = '../../test.mockchain.json';
-
-  // decode our serialized properties
-  var processID = properties.processID;
-  // var source = properties.source;
-  var command = properties.command;
-  var subprocesses = [];
-  // set request to what command we are performing
-  global.hybrixd.proc[processID].request = properties.command;
+function mine (proc, data) {
+  let mockchain;
   try {
-    var mockchain;
     if (fs.existsSync(filePath)) {
       mockchain = JSON.parse(fs.readFileSync(filePath, 'utf8'));
     } else {
       mockchain = [];
     }
+  } catch (e) {
+    proc.fail('This node does not support mockchain.');
+  }
 
-    switch (command[0]) {
-      case 'mine' :
-        var contract = command[1];
-        var target = Number(command[2]);
-        var amount = Number(command[3]);
-        var newTransaction = {type: 'mine', contract, target, amount, id: mockchain.length};
-        mockchain.push(newTransaction);
-        fs.writeFileSync(filePath, JSON.stringify(mockchain));
-        subprocesses.push('pass(' + JSON.stringify(newTransaction) + ')');
-        break;
-      case 'balance' :
-        var contract = command[1];
-        var address = Number(command[2]);
-        var balance = 0;
-        for (var i = 0; i < mockchain.length; ++i) {
-          var transaction = mockchain[i];
-          if (transaction.target === address && transaction.contract === contract) { balance += transaction.amount; }
-          if (transaction.source === address && transaction.contract === contract) { balance -= transaction.amount + transaction.fee; }
-        }
-        subprocesses.push('pass("' + balance + '")');
-        break;
-      case 'push' :
-        var newTransaction = JSON.parse(command[1]);
-        var source = newTransaction.source;
-        var target = newTransaction.target;
-        var contract = newTransaction.contract;
-        var amount = newTransaction.amount;
-        var fee = newTransaction.fee;
-        var message = typeof newTransaction.message === 'string' ? newTransaction.message : '';
-        var signature = newTransaction.signature;
-        if (signature !== source * target + amount + fee * 3.14 + contract.length * 1001 + message.length * 123) {
-          subprocesses.push("fail 'illegal signature'");
-        } else {
-          var balance = 0;
-          for (var i = 0; i < mockchain.length; ++i) {
-            var transaction = mockchain[i];
-            if (transaction.target === source && transaction.contract === contract) { balance += transaction.amount; }
-            if (transaction.source === source && transaction.contract === contract) { balance -= transaction.amount + transaction.fee; }
-          }
-          if (balance >= amount + fee) {
-            newTransaction.timestamp = Date.now();
-            newTransaction.id = mockchain.length;
-            newTransaction.type = 'tran';
-            mockchain.push(newTransaction);
-            fs.writeFileSync(filePath, JSON.stringify(mockchain));
-            subprocesses.push('pass(' + JSON.stringify(newTransaction) + ')');
-          } else {
-            subprocesses.push("fail 'insufficient balance'");
-          }
-        }
-        break;
-      case 'history' :
+  let contract = proc.command[1];
+  let target = Number(proc.command[2]);
+  let amount = Number(proc.command[3]);
+  let newTransaction = {type: 'mine', contract, target, amount, id: mockchain.length};
+  mockchain.push(newTransaction);
+  fs.writeFileSync(filePath, JSON.stringify(mockchain));
 
-        var contract = command[1];
-        var address = Number(command[2]);
-        var history = [];
-        for (let i = 0; i < mockchain.length; ++i) {
-          let transaction = mockchain[i];
-          if ((transaction.target === address || transaction.source === address) && transaction.contract === contract) {
-            var normalizedTransaction = {
-              id: transaction.id,
-              timestamp: transaction.id,
-              amount: transaction.amount,
-              symbol: transaction.contract === 'main' ? 'mock' : 'mock.' + transaction.contract,
-              fee: transaction.fee,
-              'fee-symbol': transaction.contract === 'main' ? 'mock' : 'mock.' + transaction.contract,
-              source: transaction.source,
-              target: transaction.target,
-              data: transaction.message
-            };
-            history.push(normalizedTransaction);
-          }
-        }
-        subprocesses.push('pass(' + JSON.stringify(history) + ')');
-        break;
-      case 'transaction' :
-        var transactionId = Number(command[1]);
-        if (transactionId < mockchain.length) {
-          let transaction = mockchain[transactionId];
-          let normalizedTransaction = {
-            id: transaction.id,
-            timestamp: transaction.id,
-            amount: transaction.amount,
-            symbol: transaction.contract === 'main' ? 'mock' : 'mock.' + transaction.contract,
-            fee: transaction.fee,
-            'fee-symbol': transaction.contract === 'main' ? 'mock' : 'mock.' + transaction.contract,
-            source: transaction.source,
-            target: transaction.target,
-            data: transaction.message,
-            details: {type: transaction.type}
-          };
-          subprocesses.push('pass(' + JSON.stringify(normalizedTransaction) + ')');
-        } else {
-          subprocesses.push("fail 'unknown transaction'");
-        }
-        break;
+  proc.pass(newTransaction);
+}
+
+function balance (proc, data) {
+  let mockchain;
+  try {
+    if (fs.existsSync(filePath)) {
+      mockchain = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    } else {
+      mockchain = [];
     }
   } catch (e) {
-    subprocesses.push("fail 'This node does not support mockchain.'");
+    proc.fail('This node does not support mockchain.');
   }
-  // fire the Qrtz-language program into the subprocess queue
-  scheduler.fire(processID, subprocesses);
+
+  let contract = proc.command[1];
+  let address = Number(proc.command[2]);
+  let balance = 0;
+  for (let i = 0; i < mockchain.length; ++i) {
+    let transaction = mockchain[i];
+    if (transaction.target === address && transaction.contract === contract) { balance += transaction.amount; }
+    if (transaction.source === address && transaction.contract === contract) { balance -= transaction.amount + transaction.fee; }
+  }
+  proc.pass('' + balance);
+}
+
+function push (proc, data) {
+  let mockchain;
+  try {
+    if (fs.existsSync(filePath)) {
+      mockchain = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    } else {
+      mockchain = [];
+    }
+  } catch (e) {
+    proc.fail('This node does not support mockchain.');
+  }
+
+  let newTransaction = JSON.parse(proc.command[1]);
+  let source = newTransaction.source;
+  let target = newTransaction.target;
+  let contract = newTransaction.contract;
+  let amount = newTransaction.amount;
+  let fee = newTransaction.fee;
+  let message = typeof newTransaction.message === 'string' ? newTransaction.message : '';
+  let signature = newTransaction.signature;
+  if (signature !== source * target + amount + fee * 3.14 + contract.length * 1001 + message.length * 123) {
+    proc.fail('illegal signature');
+  } else {
+    let balance = 0;
+    for (let i = 0; i < mockchain.length; ++i) {
+      let transaction = mockchain[i];
+      if (transaction.target === source && transaction.contract === contract) { balance += transaction.amount; }
+      if (transaction.source === source && transaction.contract === contract) { balance -= transaction.amount + transaction.fee; }
+    }
+    if (balance >= amount + fee) {
+      newTransaction.timestamp = Date.now();
+      newTransaction.id = mockchain.length;
+      newTransaction.type = 'tran';
+      mockchain.push(newTransaction);
+      fs.writeFileSync(filePath, JSON.stringify(mockchain));
+      proc.pass(newTransaction.id);
+    } else {
+      proc.fail('insufficient balance');
+    }
+  }
+}
+
+function history (proc, data) {
+  let mockchain;
+  try {
+    if (fs.existsSync(filePath)) {
+      mockchain = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    } else {
+      mockchain = [];
+    }
+  } catch (e) {
+    proc.fail('This node does not support mockchain.');
+  }
+
+  let contract = proc.command[1];
+  let address = Number(proc.command[2]);
+  let history = [];
+  for (let i = 0; i < mockchain.length; ++i) {
+    let transaction = mockchain[i];
+    if ((transaction.target === address || transaction.source === address) && transaction.contract === contract) {
+      let normalizedTransaction = {
+        id: transaction.id,
+        timestamp: transaction.id,
+        amount: transaction.amount,
+        symbol: transaction.contract === 'main' ? 'mock' : 'mock.' + transaction.contract,
+        fee: transaction.fee,
+        'fee-symbol': transaction.contract === 'main' ? 'mock' : 'mock.' + transaction.contract,
+        source: transaction.source,
+        target: transaction.target,
+        data: transaction.message
+      };
+      history.push(normalizedTransaction);
+    }
+  }
+  proc.pass(history);
+}
+
+function transaction (proc, data) {
+  let mockchain;
+  try {
+    if (fs.existsSync(filePath)) {
+      mockchain = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    } else {
+      mockchain = [];
+    }
+  } catch (e) {
+    proc.fail('This node does not support mockchain.');
+  }
+
+  let transactionId = Number(proc.command[1]);
+  if (transactionId < mockchain.length) {
+    let transaction = mockchain[transactionId];
+    let normalizedTransaction = {
+      id: transaction.id,
+      timestamp: transaction.id,
+      amount: transaction.amount,
+      symbol: transaction.contract === 'main' ? 'mock' : 'mock.' + transaction.contract,
+      fee: transaction.fee,
+      'fee-symbol': transaction.contract === 'main' ? 'mock' : 'mock.' + transaction.contract,
+      source: transaction.source,
+      target: transaction.target,
+      data: transaction.message,
+      details: {type: transaction.type}
+    };
+    proc.pass(normalizedTransaction);
+  } else {
+    proc.fail('unknown transaction');
+  }
 }
--- modules/update/module.js
+++ modules/update/module.js
@@ -2,65 +2,21 @@
 // hybrixd module - storage/module.js
 // Module to provide storage
 
-var scheduler = require('../../lib/scheduler');
-
-var fs = require('fs');
-var execSync = require('child_process').execSync;
-
-var projectId = '7832991'; // the hybrixd gitlab project id
+let execSync = require('child_process').execSync;
 
 // exports
-exports.init = init;
-exports.exec = exec;
 exports.update = update;
 
-// initialization function
-function init () {
-}
-
-// exec
-function exec (properties) {
-  // decode our serialized properties
-  var processID = properties.processID;
-  // var source = properties.source;
-  var command = properties.command;
-  var subprocesses = [];
-  // set request to what command we are performing
-  global.hybrixd.proc[processID].request = command;
-
-  var lastUpdated = '';
-  if (fs.existsSync('../UPDATED')) {
-    lastUpdated = fs.readFileSync('../UPDATED', 'utf8');
-    // TODO validate if it is a valid timestamp string (without line end)
-  }
-
-  subprocesses.push('curl("https://gitlab.com","/api/v4/projects/' + projectId + '/repository/commits' + (lastUpdated ? '?since=' + lastUpdated : '') + '","GET")');
-  subprocesses.push('test(data === [],1,2,data)'); // if no commits were done since last version
-  subprocesses.push('stop(0,"hybrixd is up to date.")');
-  subprocesses.push('tran(".0.committed_date",2,1)');
-  subprocesses.push('stop(1,"Could not retrieve update information.")');
-  subprocesses.push('test(data === "' + lastUpdated + '",1,2,data)');
-  subprocesses.push('stop(0,"hybrixd is up to date.")');
-  subprocesses.push('logs(0,"Update available: "+data,data)');
-  subprocesses.push("func('update',{updated:data})");
-  // Restart hybrixd
-  subprocesses.push('rout("/command/reload")');
-
-  // fire the Qrtz-language program into the subprocess queue
-  scheduler.fire(processID, subprocesses);
-}
-
-function update (data) {
+function update (proc, data) {
   execSync('rm -rf ../update/*');
   execSync('mkdir -p ../update');
 
   console.log(' [.] Retrieving archive package.');
-  execSync('curl -OL "https://gitlab.com/api/v4/projects/' + projectId + '/repository/archive.tar.gz"');
+  execSync('curl -OL "https://gitlab.com/api/v4/projects/' + data.projectId + '/repository/archive.tar.gz"');
 
   console.log(' [.] Unpacking archive.');
   execSync('tar xvf archive.tar.gz -C ../update');
   execSync('rm -f archive.tar.gz');
-  execSync('printf "' + data.updated + '" > ../UPDATED');
 
   console.log(' [.] Updating files.');
   execSync('mv -f ../update/*/* ..');
@@ -69,5 +25,5 @@ function update (data) {
   execSync('rm -f archive.tar.gz');
   execSync('rm -rf ../update/*');
 
-  scheduler.stop(data.processID, 0, 'Update succesfull');
+  proc.pass('Update succesfull');
 }
