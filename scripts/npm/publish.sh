7c4218812f6f926d4d1927232cc10fb4d24359af
“Marco <marco.alkema@gmail.com>
2019-11-13 14:54:36 +0100

    Add echo

--- scripts/npm/publish.sh
+++ scripts/npm/publish.sh
@@ -11,11 +11,13 @@ versionNumber="${version//v}"
 
 sed -i -e "s/.*version.*/\"version\": \"$versionNumber\",/" package.json
 
+echo "Publish on NPM"
 npm install npm-cli-login
 node ./node_modules/npm-cli-login/bin/npm-cli-login.js -u "$NPM_USER" -p "$NPM_PASSWORD" -e "$NPM_MAIL" -r "https://registry.npmjs.org"
 npm publish
 
 # Publish on Github
+echo "Publish on Github"
 touch .npmrc
 echo "//npm.pkg.github.com/:_authToken=$GITHUB_PAT" >> .npmrc
 echo "@hybrix-io:registry=https://npm.pkg.github.com" >> .npmrc
