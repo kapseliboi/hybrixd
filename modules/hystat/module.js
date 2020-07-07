8ebac89e1fe3f9b8450fcd340b07283cf20ad57e
Agent725 <agent725@725.be>
2020-07-07 08:20:26 +0000

    Agent725 hystat update

--- modules/hystat/module.js
+++ modules/hystat/module.js
@@ -1,7 +1,9 @@
-// (C) 2020 hybrix / Rouke Pouw
+// (C) 2020 hybrix / Rouke Pouw / Joachim de Koning
 
 const DEFAULT_RATE = '3.1546';
 const DEFAULT_VOLUME = '15635.3';
+const DEFAULT_SUPPLY = '7000000';
+const DEFAULT_SUPPLYATOM = '7000000000000000000000000';
 
 function hystat (proc) {
   const command = proc.command;
@@ -9,10 +11,11 @@ function hystat (proc) {
   proc.mime('text/plain');
   switch (endpoint) {
     case 'maximumSupply': proc.done('21000000'); return;
-    case 'circulatingSupply': proc.done('7000000.00'); return;
+    case 'reservedSupply': proc.done(proc.peek('local::reservedSupply', DEFAULT_SUPPLY)); return;
+    case 'circulatingSupply': proc.done(proc.peek('local::circulatingSupply', DEFAULT_SUPPLY)); return;
+    case 'circulatingSupplyAtom': proc.done(proc.peek('local::circulatingSupplyAtom', DEFAULT_SUPPLYATOM)); return;
+    case 'circulatingSupplyWithoutReserve': proc.done(proc.peek('local::circulatingSupplyWithoutReserve', DEFAULT_SUPPLY)); return;
     case 'supplyFactor': proc.done('18'); return;
-    case 'circulatingSupplyAtom': proc.done('21000000000000000000000000'); return;
-    case 'circulatingSupplyWithoutReserve': proc.done('6000000.00'); return;
     case 'currentPrice': proc.done(proc.peek('local::rate', DEFAULT_RATE)); return;
     case 'currentVolume': proc.done(proc.peek('local::volume', DEFAULT_VOLUME)); return;
   }
