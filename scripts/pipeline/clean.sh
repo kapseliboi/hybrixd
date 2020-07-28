db1ceb44486009150a99a0ac2a8409b41118d7fe
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2020-07-28 10:34:26 +0200

    fix hybrixd.conf 1111 values

--- scripts/pipeline/clean.sh
+++ scripts/pipeline/clean.sh
@@ -8,4 +8,11 @@ rm README.npm.md || true
 echo "[.] Remove pipeline scripts"
 rm -rf ./scripts/pipeline
 
+echo "[.] Reset hybrixd.conf"
+echo "[host]" >  hybrixd.conf
+echo 'servers = { "http://127.0.0.1:1111" : "/root", "http://127.0.0.1:8080" : "/source/web-wallet", "http://127.0.0.1:8090" : "/source/web-blockexplorer"}' >>  hybrixd.conf
+
+export PATH="$OLDPATH"
+cd "$WHEREAMI"
+
 exit 0
