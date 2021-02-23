9f7aa1b4461938cc642a4e68e0a309d660ac7e4b
Rouke Pouw <roukepouw@gmail.com>
2021-02-23 12:51:31 +0000

    Improve valuation engine

--- modules/valuations/parse.js
+++ modules/valuations/parse.js
@@ -1,12 +1,13 @@
-const xmldoc = require('xmldoc');
 
 function addQuote (accumulator, quoteCurrency, baseCurrency, price) {
   // Don't add quote if the input fails these requirements
+  quoteCurrency = quoteCurrency.toUpperCase();
+  baseCurrency = baseCurrency.toUpperCase();
   if (quoteCurrency === baseCurrency || price === 0 || !isFinite(price)) return accumulator;
 
   if (!accumulator.hasOwnProperty(quoteCurrency)) accumulator[quoteCurrency] = {quotes: {}};
 
-  accumulator[quoteCurrency]['quotes'][baseCurrency] = price;
+  accumulator[quoteCurrency].quotes[baseCurrency] = price;
   return accumulator;
 }
 
@@ -15,31 +16,12 @@ function addBiQuote (accumulator, quoteCurrency, baseCurrency, price) {
   addQuote(accumulator, baseCurrency, quoteCurrency, 1 / price);
 }
 
-function parseEUCentralBank (xmlString) {
-  const name = 'EUCentralBank';
-  let xmlElements;
-  try {
-    const document = new xmldoc.XmlDocument(xmlString);
-    xmlElements = document.children[5].children[1].children.filter(node => node.constructor.name === 'XmlElement');
-  } catch (e) {
-    return {name: name, quotes: {}};
-  }
-  const quoteAccumulator = {};
-  xmlElements.map(function (key, index) {
-    const price = key.attr.rate;
-    const quoteCurrency = key.attr.currency;
-
-    addBiQuote(quoteAccumulator, 'EUR', quoteCurrency, price);
-  });
-  return {name: name, quotes: quoteAccumulator};
-}
-
 function parseCoinmarketcap (obj) {
   const name = 'coinmarketcap';
   const quoteAccumulator = {};
   if (typeof obj !== 'object' || obj === null || !obj.hasOwnProperty('data')) return {name, quotes: quoteAccumulator};
 
-  Object.keys(obj.data).map(function (key, index) {
+  Object.keys(obj.data).forEach(function (key, index) {
     const price = obj.data[key].quotes.USD.price;
     const quoteCurrency = obj.data[key].symbol;
 
@@ -64,7 +46,7 @@ function parseCoinbase (obj) {
   const quoteAccumulator = {};
   if (typeof obj !== 'object' || obj === null) return {name, quotes: quoteAccumulator};
 
-  Object.keys(obj.data.rates).map(function (key, index) {
+  Object.keys(obj.data.rates).forEach(function (key, index) {
     const price = parseFloat(obj.data.rates[key]);
 
     addBiQuote(quoteAccumulator, 'USD', key, price);
@@ -78,7 +60,7 @@ function parseBinance (obj) {
   const quoteAccumulator = {};
   if (typeof obj !== 'object' || obj === null) return {name, quotes: quoteAccumulator};
 
-  Object.keys(obj).map(function (key, index) {
+  Object.keys(obj).forEach(function (key, index) {
     const symbolPair = obj[key].symbol;
     const baseCurrency = baseCurrencies.find(function (currency) { return symbolPair.endsWith(currency); });
     if (baseCurrency) {
@@ -98,14 +80,14 @@ function parseHitbtc (prices, symbols) {
   if (!(prices instanceof Array) || !(symbols instanceof Array)) return {name, quotes: quoteAccumulator};
 
   const symbolsObj = {};
-  symbols.map(function (key) {
+  symbols.forEach(function (key) {
     symbolsObj[key.id] = key;
   });
 
-  prices.map(function (key, index) {
-    const exchangeSymbol = symbolsObj[key['symbol']];
-    const baseCurrency = exchangeSymbol['baseCurrency'];
-    const quoteCurrency = exchangeSymbol['quoteCurrency'];
+  prices.forEach(function (key, index) {
+    const exchangeSymbol = symbolsObj[key.symbol];
+    const baseCurrency = exchangeSymbol.baseCurrency;
+    const quoteCurrency = exchangeSymbol.quoteCurrency;
     const price = parseFloat(key.last);
 
     addBiQuote(quoteAccumulator, baseCurrency, quoteCurrency, price);
@@ -113,18 +95,26 @@ function parseHitbtc (prices, symbols) {
   return {name: name, quotes: quoteAccumulator};
 }
 
+function parseDefault (data, name) {
+  // baseCurrencies are included in the downloaded data
+  const quoteAccumulator = {};
+  if (!(data instanceof Array)) return {name, quotes: quoteAccumulator};
+  for (const pair of data) addBiQuote(quoteAccumulator, pair.from, pair.to, Number(pair.price));
+  return {name: name, quotes: quoteAccumulator};
+}
+
 function combineQuoteSources (sources) {
   const combinedQuotes = sources.reduce(function (accumulatedSources, newSource) {
     const newQuotes = newSource.quotes;
-    Object.keys(newQuotes).map(function (quoteCurrency, index) {
+    Object.keys(newQuotes).forEach(function (quoteCurrency, index) {
       if (!accumulatedSources.hasOwnProperty(quoteCurrency)) {
         accumulatedSources[quoteCurrency] = {quotes: {}};
       }
-      Object.keys(newQuotes[quoteCurrency]['quotes']).map(function (baseCurrency, index) {
-        if (!accumulatedSources[quoteCurrency]['quotes'].hasOwnProperty(baseCurrency)) {
-          accumulatedSources[quoteCurrency]['quotes'][baseCurrency] = {sources: {}};
+      Object.keys(newQuotes[quoteCurrency].quotes).forEach(function (baseCurrency, index) {
+        if (!accumulatedSources[quoteCurrency].quotes.hasOwnProperty(baseCurrency)) {
+          accumulatedSources[quoteCurrency].quotes[baseCurrency] = {sources: {}};
         }
-        accumulatedSources[quoteCurrency]['quotes'][baseCurrency]['sources'][newSource['name']] = newQuotes[quoteCurrency]['quotes'][baseCurrency];
+        accumulatedSources[quoteCurrency].quotes[baseCurrency].sources[newSource.name] = newQuotes[quoteCurrency].quotes[baseCurrency];
       });
     });
     return accumulatedSources;
@@ -134,24 +124,24 @@ function combineQuoteSources (sources) {
 }
 
 function updateMinAndMedians (exchangeRates) {
-  Object.keys(exchangeRates).map(function (sourceCurrency, _) {
-    const quotes = exchangeRates[sourceCurrency]['quotes'];
-    Object.keys(quotes).map(function (targetCurrency, _) {
-      const exchanges = quotes[targetCurrency]['sources'];
+  Object.keys(exchangeRates).forEach(function (sourceCurrency, _) {
+    const quotes = exchangeRates[sourceCurrency].quotes;
+    Object.keys(quotes).forEach(function (targetCurrency, _) {
+      const exchanges = quotes[targetCurrency].sources;
       const sortedExchanges = Object.keys(exchanges).sort(function (exchange1, exchange2) {
         return exchanges[exchange2] - exchanges[exchange1];
       });
 
-      exchangeRates[sourceCurrency]['quotes'][targetCurrency]['highest_rate'] = {'exchange': sortedExchanges[0], rate: exchanges[sortedExchanges[0]]};
+      exchangeRates[sourceCurrency].quotes[targetCurrency].highest_rate = {exchange: sortedExchanges[0], rate: exchanges[sortedExchanges[0]]};
       const lowPoint = sortedExchanges.length - 1;
-      exchangeRates[sourceCurrency]['quotes'][targetCurrency]['lowest_rate'] = {'exchange': sortedExchanges[lowPoint], rate: exchanges[sortedExchanges[lowPoint]]};
+      exchangeRates[sourceCurrency].quotes[targetCurrency].lowest_rate = {exchange: sortedExchanges[lowPoint], rate: exchanges[sortedExchanges[lowPoint]]};
       const midPoint = (sortedExchanges.length - 1) / 2;
       const floorExchange = sortedExchanges[Math.floor(midPoint)];
       const ceilExchange = sortedExchanges[Math.ceil(midPoint)];
 
       const exchange = floorExchange === ceilExchange ? ceilExchange : (floorExchange + '|' + ceilExchange);
 
-      exchangeRates[sourceCurrency]['quotes'][targetCurrency]['median_rate'] = {
+      exchangeRates[sourceCurrency].quotes[targetCurrency].median_rate = {
         exchange,
         rate: (Number(exchanges[floorExchange]) + Number(exchanges[ceilExchange])) / 2
       };
@@ -162,12 +152,14 @@ function updateMinAndMedians (exchangeRates) {
 
 function parse (proc, data) {
   const sourcesOut = combineQuoteSources([
-    parseEUCentralBank(data.EUCentralBank),
+    parseDefault(data.EUCentralBank, 'EUCentralBank'),
     parseHitbtc(data.hitbtc_symbols, data.hitbtc_prices),
     parseBinance(data.binance),
     parseCoinmarketcap(data.coinmarketcap),
     parseCoinbase(data.coinbase),
-    parseBiki(data.biki_hyusdt)
+    parseBiki(data.biki_hyusdt),
+    parseDefault(data.uni_swap, 'uni_swap'),
+    parseDefault(data.tomo_dex, 'tomo_dex')
   ]);
   const result = updateMinAndMedians(sourcesOut);
   //  store hy volume
@@ -175,6 +167,15 @@ function parse (proc, data) {
     const hyVolume = data.biki_hyusdt.data.vol;
     proc.poke('local::hy-volume', hyVolume);
   }
+  const symbols = [];
+  for (const symbol1 in result) {
+    if (!symbols.includes(symbol1)) symbols.push(symbol1);
+    for (const symbol2 in result[symbol1].quotes) {
+      if (!symbols.includes(symbol2)) symbols.push(symbol2);
+    }
+  }
+  proc.poke('local::symbols', symbols);
+
   proc.done(result);
 }
 
