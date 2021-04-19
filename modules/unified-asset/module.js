27a1020c6a0b2c5deefc002df7cc18af7f594c7b
RoukePouw <34579399+RoukePouw@users.noreply.github.com>
2021-04-19 08:46:41 +0200

    Upgrade unified module

--- modules/unified-asset/module.js
+++ modules/unified-asset/module.js
@@ -6,18 +6,27 @@
 const compressUnifiedAddress = require('../../common/compress-unified-address');
 
 // functions
-function compress (proc, data) {
-  proc.done(compressUnifiedAddress.encode(data));
+function compress (proc, address) {
+  const symbol = proc.command[1];
+  proc.done(symbol + ':' + compressUnifiedAddress.encode(address));
 }
 
 function decompress (proc, address) {
-  const symbol = proc.peek('symbol');
-  if (address.startsWith(symbol + ':')) { // `${symbol}:${encoded}`
+  const symbol = proc.command[1];
+  if(!symbol || symbol === 'undefined') return proc.done(tryDecompress(address));
+  else if (address.startsWith(symbol + ':')) { // `${symbol}:${encoded}`
     const encoded = address.split(':')[1]; // `${symbol}:${encoded}` -> encoded
     const decoded = compressUnifiedAddress.decode(encoded);
     if (decoded === null) return proc.fail('Could not parse unified asset address!');
     else return proc.done(decoded);
   } else return proc.done(address); // `${subSymbol}:${subAddress},...`
+}
+
+function tryDecompress(address){
+  if(!address.includes(':')) return address;
+  const encoded = address.split(':')[1]; // `${symbol}:${encoded}` -> encoded
+  const decoded = compressUnifiedAddress.decode(encoded);
+  return decoded === null ? address : decoded; // fallback to undecompressable address
 }
 
 // exports
